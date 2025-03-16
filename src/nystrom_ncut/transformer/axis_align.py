b3fd8756e67e8f811a33a0a78ceaaae457dc8e40
JophiArcana <wentinn.liao@gmail.com>
2025-03-15 20:03:22 -0400

    Implemented multi-batch AxisAlign which supports heterogeneous sequence lengths.

--- src/nystrom_ncut/transformer/axis_align.py
+++ src/nystrom_ncut/transformer/axis_align.py
@@ -3,6 +3,9 @@ from typing import Literal
 import torch
 import torch.nn.functional as Fn
 
+from ..common import (
+    default_device,
+)
 from .transformer_mixin import (
     TorchTransformerMixin,
 )
@@ -27,50 +30,59 @@ class AxisAlign(TorchTransformerMixin):
 
     def fit(self, X: torch.Tensor) -> "AxisAlign":
         # Normalize eigenvectors
-        n, d = X.shape
-        normalized_X = Fn.normalize(X, p=2, dim=-1)
+        with default_device(X.device):
+            d = X.shape[-1]
+            normalized_X = Fn.normalize(X, p=2, dim=-1)                                                         # float: [... x n x d]
 
-        # Initialize R matrix with the first column from a random row of EigenVectors
-        self.R = torch.empty((d, d), device=X.device)
-        self.R[0] = normalized_X[torch.randint(0, n, (), device=X.device)]
+            # Initialize R matrix with the first column from a random row of EigenVectors
+            def get_idx(idx: torch.Tensor) -> torch.Tensor:
+                return torch.gather(normalized_X, -2, idx[..., None, None].expand([-1] * (X.ndim - 2) + [1, d]))[..., 0, :]
 
-        # Loop to populate R with k orthogonal directions
-        c = torch.zeros((n,), device=X.device)
-        for i in range(1, d):
-            c += torch.abs(normalized_X @ self.R[i - 1])
-            self.R[i] = normalized_X[torch.argmin(c, dim=0)]
+            self.R = torch.empty((*X.shape[:-2], d, d))                                                         # float: [... x d x d]
+            mask = torch.all(torch.isfinite(normalized_X), dim=-1)                                              # bool: [... x n]
+            start_idx = torch.argmax(mask.to(torch.float) + torch.rand(mask.shape), dim=-1)                     # int: [...]
+            self.R[..., 0, :] = get_idx(start_idx)
 
-        # Iterative optimization loop
-        idx, prev_objective = None, torch.inf
-        for _ in range(self.max_iter):
-            # Discretize the projected eigenvectors
-            idx = torch.argmax(normalized_X @ self.R.mT, dim=-1)
-            M = torch.zeros((d, d), device=X.device).index_add_(0, idx, normalized_X)
+            # Loop to populate R with k orthogonal directions
+            c = torch.zeros(X.shape[:-1])                                                                       # float: [... x n]
+            for i in range(1, d):
+                c += torch.abs(normalized_X @ self.R[..., i - 1, :, None])[..., 0]
+                self.R[..., i, :] = get_idx(torch.argmin(c.nan_to_num(nan=torch.inf), dim=-1))
 
-            # Check for convergence
-            objective = torch.norm(M)
-            if torch.abs(objective - prev_objective) < torch.finfo(torch.float32).eps:
-                break
-            prev_objective = objective
+            # Iterative optimization loop
+            normalized_X = torch.nan_to_num(normalized_X, nan=0.0)
+            idx, prev_objective = None, torch.inf
+            for _ in range(self.max_iter):
+                # Discretize the projected eigenvectors
+                idx = torch.argmax(normalized_X @ self.R.mT, dim=-1)                                                    # int: [... x n]
+                M = torch.sum((idx[..., None] == torch.arange(d))[..., None] * normalized_X[..., :, None, :], dim=-3)   # float: [... x d x d]
 
-            # SVD decomposition to compute the next R
-            U, S, Vh = torch.linalg.svd(M, full_matrices=False)
-            self.R = U @ Vh
+                # Check for convergence
+                objective = torch.norm(M)
+                if torch.abs(objective - prev_objective) < torch.finfo(torch.float32).eps:
+                    break
+                prev_objective = objective
 
-        # Permute the rotation matrix so the dimensions are sorted in descending cluster significance
-        if self.sort_method == "count":
-            sort_metric = torch.bincount(idx, minlength=d)
-        elif self.sort_method == "norm":
-            rotated_X = X @ self.R.mT
-            sort_metric = torch.linalg.norm(rotated_X, dim=0)
-        elif self.sort_method == "marginal_norm":
-            rotated_X = X @ self.R.mT
-            sort_metric = torch.zeros((d,), device=X.device).index_add_(0, idx, rotated_X[range(n), idx] ** 2)
-        else:
-            raise ValueError(f"Invalid sort method {self.sort_method}.")
+                # SVD decomposition to compute the next R
+                U, S, Vh = torch.linalg.svd(M, full_matrices=False)
+                self.R = U @ Vh
 
-        self.R = self.R[torch.argsort(sort_metric, dim=0, descending=True)]
-        return self
+            # Permute the rotation matrix so the dimensions are sorted in descending cluster significance
+            match self.sort_method:
+                case "count":
+                    sort_metric = torch.sum((idx[..., None] == torch.arange(d)), dim=-2)
+                case "norm":
+                    rotated_X = torch.nan_to_num(X @ self.R.mT, nan=0.0)
+                    sort_metric = torch.linalg.norm(rotated_X, dim=-2)
+                case "marginal_norm":
+                    rotated_X = torch.nan_to_num(X @ self.R.mT, nan=0.0)
+                    sort_metric = torch.sum((idx[..., None] == torch.arange(d)) * (torch.gather(rotated_X, -1, idx[..., None]) ** 2), dim=-2)
+                case _:
+                    raise ValueError(f"Invalid sort method {self.sort_method}.")
+
+            order = torch.argsort(sort_metric, dim=-1, descending=True)
+            self.R = torch.gather(self.R, -2, order[..., None].expand([-1] * order.ndim + [d]))
+            return self
 
     def transform(self, X: torch.Tensor, normalize: bool = True, hard: bool = False) -> torch.Tensor:
         """
@@ -82,9 +94,9 @@ class AxisAlign(TorchTransformerMixin):
             torch.Tensor: Discretized eigenvectors, shape (n, k), each row is a one-hot vector.
         """
         if normalize:
-            X = Fn.normalize(X, p=2, dim=1)
+            X = Fn.normalize(X, p=2, dim=-1)
         rotated_X = X @ self.R.mT
-        return torch.argmax(rotated_X, dim=1) if hard else rotated_X
+        return torch.argmax(rotated_X, dim=-1) if hard else rotated_X
 
     def fit_transform(self, X: torch.Tensor, normalize: bool = True, hard: bool = False) -> torch.Tensor:
         return self.fit(X).transform(X, normalize=normalize, hard=hard)
